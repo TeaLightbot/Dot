cc90f786e5661e3f45efb96dd78fed2a7d08429b
Johnny Thow <johnnyfoe@gmail.com>
2015-09-16 13:36:52 +0100

    Added .featureRequests command

--- modules/featureRequest/actions.js
+++ modules/featureRequest/actions.js
@@ -1,5 +1,8 @@
 'use strict';
 var FeatureRequest = require('./model');
+var os             = require("os");
+var config         = require('../../setup/config.json');
+var util           = require("util");
 
 (function(actions){
     actions.store = function(bot, from, to, text, split, sendTo) {
@@ -16,5 +19,17 @@ var FeatureRequest = require('./model');
             bot.emit('response', err || 'Your call is important to us, thank you for holding.', sendTo);
         });
 	};
+
+    actions.url = function(bot, from, to, text, split, sendTo){
+        var hostname = os.hostname();
+
+        var response = util.format("Current list viewable here (assuming network connectivity): " +
+            "http://%s:%s/featureRequests",
+            hostname,
+            config.httpPort);
+
+        // fetch feature request URL
+        bot.emit('response', response, sendTo);
+    };
 
 })(module.exports);
